c83adbfa5727b480aa7cc98910982b7185b889c6
JaJa12290 <jandersongsantos2@gmail.com>
2023-10-29 16:48:42 -0400

    Backup 2 do Trabalho

--- Trabalho 2/src/servico/BaseServico.java
+++ Trabalho 2/src/servico/BaseServico.java
@@ -1,5 +1,20 @@
 package servico;
 
-public class BaseServico {
+import java.util.ArrayList;
+
+public abstract class BaseServico<TRepositorio, TDominio> {
       
+    protected TRepositorio repositorio;
+
+    public abstract TDominio Inserir(TDominio inserir);
+
+    public abstract TDominio Obter(int codigo);
+
+    public abstract ArrayList<TDominio> Listar();
+
+    public abstract TDominio Atualizar(TDominio inserir);
+
+    public abstract TDominio Excluir(int codigo);
+
+    public BaseServico(){}
 }
